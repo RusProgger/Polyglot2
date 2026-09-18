62d38597983abcdf9cb64251125cf21f747dad0f
RusProgger <ruslantimka92@gmail.com>
2026-09-18 14:55:14 +0300

    Send form modal

--- send-callback.php
+++ send-callback.php
@@ -1,37 +1,48 @@
 <?php
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    http_response_code(405);
-    exit('Метод не разрешен');
+    exit('Ошибка запроса');
 }
 
-$phone = $_POST['phone'] ?? '';
+$phone = trim($_POST['phone'] ?? '');
+
+if (!$phone) {
+    exit('Нет телефона');
+}
+
 
 $phone = preg_replace('/\D/', '', $phone);
 
-if (!preg_match('/^\d{9}$/', $phone)) {
-    http_response_code(400);
-    exit('Неверный номер телефона');
+
+if (strlen($phone) !== 9) {
+    exit('Неверный номер');
 }
 
-// ВАША ПОЧТА
-$to = 'mailto:juliaads22@gmail.com';
 
-$fullPhone = '+380' . $phone;
+$phone = '+380' . $phone;
 
-$subject = 'Новая заявка на обратный звонок';
 
-$message = "Новая заявка с сайта.\n\n";
-$message .= "Номер телефона: " . $fullPhone . "\n";
-$message .= "Дата: " . date('d.m.Y H:i:s') . "\n";
+$to = 'kakoito@mail.ru';
 
-$headers = "From: website@your-domain.com\r\n";
-$headers .= "Reply-To: website@your-domain.com\r\n";
-$headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+$subject = 'Заявка на обратный звонок';
 
-if (mail($to, $subject, $message, $headers)) {
-    echo 'success';
+$message = "Новая заявка на обратный звонок\n\n";
+$message .= "Телефон: " . $phone;
+
+
+$headers = [];
+$headers[] = 'From: info@polyglot-translate.org.ua';
+$headers[] = 'Reply-To: info@polyglot-translate.org.ua';
+$headers[] = 'MIME-Version: 1.0';
+$headers[] = 'Content-Type: text/plain; charset=UTF-8';
+
+
+if (mail($to, $subject, $message, implode("\r\n", $headers))) {
+
+    echo "success";
+
 } else {
-    http_response_code(500);
-    echo 'Ошибка отправки';
+
+    echo "error";
+
 }
